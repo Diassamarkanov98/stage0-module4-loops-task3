b2b2f0707834f10500ede0cccf396492e98b478b
Dias <diassamarkanov108@gmail.com>
2023-02-27 20:37:00 +0600

    init: start module 4 loops task 3

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int n1=0 , n2=1 , n3 , i , count=lastFibonacci;
+        System.out.println(n1);
+        System.out.println(n2);
+        for( i=2 ; i<count ; ++i) {
+            n3=n1+n2;
+            System.out.println(n3);
+            n1=n2;
+            n2=n3;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        if (lengthOfLastNumber < 0){
+            System.out.println(0);
+        } else {
+            int ten_pow = 1;
+            int result = 0;
+            for(int i = 1; i <= lengthOfLastNumber; i++){
+                ten_pow *= 10;
+                result += ten_pow ;
+            }
+            result -= lengthOfLastNumber;
+            System.out.println(result);
+        }
     }
 }
